8a7c60af3235372e67f01e86c9951d669a3c3a83
Dudi001 <murad.manapov.96@gmail.com>
2021-02-04 23:05:58 +0300

    othe

--- homework.py
+++ homework.py
@@ -1,21 +1,39 @@
+import os
 import time
 
 import requests
+from dotenv import load_dotenv
 from twilio.rest import Client
 
-# здесь проинициализируйте Client 
+load_dotenv()
+
+BASE_URL = 'https://api.vk.com/method/users.get'
+
 
 def get_status(user_id):
     params = {
-        ...
+        'user_ids': user_id,
+        'V': os.getenv('API_V'),
+        'access_token': os.getenv('ACCESS_TOKEN'),
+        'fields': 'online'
     }
-    ...
-    return ...  # Верните статус пользователя в ВК
+    friends_status = requests.post(BASE_URL, params=params)
+
+    return friends_status.json()['response'][0]['online']
 
 
 def send_sms(sms_text):
-    ...
-    return ...  # Верните sid отправленного сообщения из Twilio
+    account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        body='Joing the dark side',
+        from_=os.getenv('NUMBER_FROM'),
+        media_url=['https://demo.twilio.com/owl.png'],
+        to=os.getenv('NUMBER_TO')
+    )
+    return message.sid
 
 
 if __name__ == '__main__':
